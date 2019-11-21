758e5be7951eb148e56fe2eacfc5b33f9c17af2e
shadownova65 <pyyanguw@gmail.com>
2019-11-21 10:10:03 +0800

    simple bar chart

--- src/BarChart.jsx
+++ src/BarChart.jsx
@@ -1,33 +1,54 @@
 import React, {useEffect, useState} from 'react';
-import * as d3 from 'react-d3-library';
+import * as d3 from 'd3';
 
 function BarChart() {
-    const initialBooks = [
-        {
-            name: "Harry Potter",
-            author: "J. K. Rowling",
-            genre: "fantasy",
-        }, {
-            name: "The PEdagogy of Freedom",
-            author: "Bell Hooks",
-            genre: "non-ficiton"
-        }, {
-            name: "Harry Potter 2",
-            author: "J. K. Rowling",
-            genre: "fantasy"
-        }, {
-            name: "Gilgamesh",
-            author: "Derrek Hines",
-            genre: "poetry"
-        }
-    ]
-    const [books, setBooks] = useState(initialBooks);
+    //* Keywords
+
+     // const initialBooks = [
+    //     {
+    //         name: "Harry Potter",
+    //         author: "J. K. Rowling",
+    //         genre: "fantasy",
+    //     }, {
+    //         name: "The PEdagogy of Freedom",
+    //         author: "Bell Hooks",
+    //         genre: "non-ficiton"
+    //     }, {
+    //         name: "Harry Potter 2",
+    //         author: "J. K. Rowling",
+    //         genre: "fantasy"
+    //     }, {
+    //         name: "Gilgamesh",
+    //         author: "Derrek Hines",
+    //         genre: "poetry"
+    //     }
+    // ]
+    // const [books, setBooks] = useState(initialBooks);
+
+    const [data, setData] = useState([30, 86, 168, 281, 303, 365]);
+
+    useEffect(() => {
+        //render a simple barchart
+        d3.select('.simple-barchart')
+        .selectAll('div')
+        .data(data)
+        .enter()
+        .append('div')
+        .style("width", function(d) { return `${d}px` })
+        .text(function(d) { return d })
+    }, [])
+
+   
 
     return (
         <div>
-            <h1>
-                This will render a bar chart
-            </h1>
+            <h2>
+                This will render some bar charts
+            </h2>
+
+            <div className="simple-barchart">
+                <h3>A simple bar chart</h3>
+            </div>
 
         </div>
     )
